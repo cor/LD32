d97b754beb4e37ced3ebb3cc6e7b9aedc0684b19
CorPruijs <cor@pruys.com>
2015-04-18 11:29:20 +0200

    Added a very basic PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed = 4.0f;
+	public string horizontalAxisName = "Horizontal";
+	// Use this for initialization
+	void Start () {                             
+	}
+	
+	void FixedUpdate () {
+
+		float currentAxisValue = Input.GetAxis(horizontalAxisName);
+
+		if (currentAxisValue > 0) {
+			//move right
+			Debug.Log("right" + currentAxisValue);
+			transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+
+		}
+
+		else if (currentAxisValue < 0) {
+			//move left
+			Debug.Log("left" + currentAxisValue);
+			transform.Translate(Vector2.right * -movementSpeed * Time.deltaTime);
+		}
+	
+	}
+}
