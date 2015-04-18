c669c58c577a5c6a621d0499806756a52f75427c
CorPruijs <cor@pruys.com>
2015-04-18 12:19:11 +0200

    The smooth Follow script is actually smooth now

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -4,17 +4,24 @@ using System.Collections;
 public class SmoothFollow : MonoBehaviour {
 
 	public GameObject target;
+	private Vector3 velocity = Vector3.zero;
+	public float dampTime = 0.15f;
 
-	Vector3 deltaPosition;
+//	Vector3 deltaPosition
 
 	// Use this for initialization
 	void Start () {
-		deltaPosition = transform.position -target.transform.position;
+//		deltaPosition = transform.position -target.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = target.transform.position + deltaPosition;
+
+		Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.transform.position);
+
+		Vector3 deltaPosition = target.transform.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+		Vector3 destination = transform.transform.position + deltaPosition;
+		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime); 
 
 	}
 }
