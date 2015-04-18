5534d86da70ce4da51b38e2400689e04cabef1ed
CorPruijs <cor@pruys.com>
2015-04-18 12:57:44 +0200

    Improved player movement a lot, still a little jittery though

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,27 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float movementSpeed = 4.0f;
-	public float jumpHeight = 5.0f;
+	public float jumpHeight = 3.0f;
 	public string horizontalAxisName = "Horizontal";
+
+	private float currentAxisValue = 0f;
+
 	// Use this for initialization
 	void Start () {                             
+	}
+
+	void Update () {
+		currentAxisValue = Input.GetAxis(horizontalAxisName);
 	}
 	
 	void FixedUpdate () {
 
-		float currentAxisValue = Input.GetAxis(horizontalAxisName);
-
-		if (currentAxisValue > 0) {
-			//move right
-			transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
-
-		}
-
-		else if (currentAxisValue < 0) {
-			//move left
-			transform.Translate(Vector2.right * -movementSpeed * Time.deltaTime);
-		}
+		transform.Translate(Vector2.right * movementSpeed * Time.deltaTime * currentAxisValue);
 
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			//jump
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse );
+
 		}
 	
 	}
