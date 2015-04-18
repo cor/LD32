bece3dddc46a4ece5adb32536f9443b328eba4d9
CorPruijs <cor@pruys.com>
2015-04-18 19:49:06 +0200

    Removed all water related code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,29 +3,65 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float movementSpeed = 4.0f;
-	public float jumpHeight = 3.0f;
-	public string horizontalAxisName = "Horizontal";
+	public float maxSpeed = 10f;
 
-	private float currentAxisValue = 0f;
+	//components
+	Animator anim;
+	Rigidbody2D rb;
+
+	bool facingRight = true;
+	bool onGround = false;
+
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 100f;
+
 
 	// Use this for initialization
-	void Start () {                             
-	}
-
-	void Update () {
-		currentAxisValue = Input.GetAxis(horizontalAxisName);
+	void Start () {
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
+	// Update is called once per frame
 	void FixedUpdate () {
 
-		transform.Translate(Vector2.right * movementSpeed * Time.deltaTime * currentAxisValue);
+		float moveAxisValue = Input.GetAxis("Horizontal");
 
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			//jump
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse );
+		onGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
+		// set animator values
+		anim.SetBool("Ground", onGround);
+		anim.SetFloat("VerticalSpeed", rb.velocity.y);
+		anim.SetFloat("HorizontalSpeed", Mathf.Abs(moveAxisValue));
+
+		rb.velocity = new Vector2(moveAxisValue * maxSpeed, rb.velocity.y); 
+
+		// flip the transform when we're facing the wrong direction
+		if (moveAxisValue > 0  && !facingRight)  {
+			// walking right, facing left
+			Flip ();
 		}
-	
+		else if (moveAxisValue < 0 && facingRight) {
+			// walking left, facing right
+			Flip ();
+		}
 	}
+
+	void Update() {
+		if (onGround && Input.GetAxis("Jump") == 1) {
+			// Jump
+			anim.SetBool("Ground", false);
+			rb.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1; // flip the transform
+		transform.localScale = scale;
+	}
+
 }
