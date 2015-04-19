6aae31b263e00ab3f8f7740de8579e7a2a3fdcb9
CorPruijs <cor@pruys.com>
2015-04-19 11:02:21 +0200

    Added enemy to the game, last commit before transfer to sublime text

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
